715eb79b7d260c7c14a605d9d8f8fbaa87bb7c97
Nicolò <vincinicolo98@gmail.com>
2022-11-09 08:53:34 +0100

    Handle Python Indentation

--- src/obfuscator/test.py
+++ src/obfuscator/test.py
@@ -2,14 +2,14 @@
 
 
 def main() -> None:
-    print("test")
-    # comment
-    '''
-    other
-    comments
-    '''
-    print("hello world")
+	print("test")
+	# comment
+	'''
+	other
+	comments
+	'''
+	print("hello world")
 
 
 if __name__ == '__main__':
-    main()
+	main()
